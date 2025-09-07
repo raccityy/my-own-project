01f38affda4022a9e9f9d0847588269d5c5b8804
raccityy <145439409+raccityy@users.noreply.github.com>
2025-09-07 06:07:05 +0600

    Update bot_instance.py

--- bot_instance.py
+++ bot_instance.py
@@ -1,7 +1,8 @@
 import telebot
 from telebot import TeleBot
 
-bot_token = "8043726162:AAGHwypAYvxQ5h5L_G7m9_50vWAJrAy1bvk"
+bot_token = "8328219962:AAF5cIY040RgWMQ7kY2yd57Y65IVIZ576S8"
 
 
 bot = telebot.TeleBot(bot_token)
+
